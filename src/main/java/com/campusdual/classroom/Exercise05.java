def06bd941b1f969e4e0c7dcd5a8d6dc4d6a590a
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2024-10-28 15:10:37 +0100

    Refine solution for Exercise05

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -1,34 +1,37 @@
 package com.campusdual.classroom;
 
 public class Exercise05 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    checkLessOrGreaterEqual(1, 0);
+    checkLessOrGreaterEqual(1, 1);
+    checkLessOrGreaterEqual(1, 2);
+    checkMultiply(4, 2);
+    checkMultiply(4, 3);
+    checkPositiveNegative(1);
+    checkPositiveNegative(-1);
+  }
 
+  public static void checkLessOrGreaterEqual(int n1, int n2) {
+    if (n1 < n2) {
+      System.out.println(n1 + " es menor que " + n2);
+    } else {
+      System.out.println(n1 + " es mayor o igual que " + n2);
     }
+  }
 
-    public static void checkLessOrGreaterEqual(int n1, int n2) {
-        if (n1 < n2) {
-            System.out.println("X es menor que Y");
-        }
-        else {
-            System.out.println("X es mayor o igual que Y");
-        }
+  public static void checkMultiply(int n1, int n2) {
+    if (Math.floor((double) n1 / (double) n2) == (double) n1 / (double) n2) {
+      System.out.println(n1 + " es múltiplo de " + n2);
+    } else {
+      System.out.println(n1 + " no es múltiplo de " + n2);
     }
+  }
 
-    public static void checkMultiply(int n1, int n2) {
-        if (Math.floor((double)n1 / (double)n2) == (double)n1 / (double)n2) {
-            System.out.println("X es múltiplo de Y");
-        }
-        else {
-            System.out.println("X no es múltiplo de Y");
-        }
+  public static void checkPositiveNegative(int num) {
+    if (num == Math.abs(num)) {
+      System.out.println(num + " es positivo.");
+    } else {
+      System.out.println(num + " es negativo.");
     }
-
-    public static void checkPositiveNegative(int num) {
-        if (num == Math.abs(num)) {
-            System.out.println("X es positivo.");
-        }
-        else {
-            System.out.println("X es negativo.");
-        }
-    }
+  }
 }
